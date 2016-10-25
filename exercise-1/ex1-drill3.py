3739faf4a5ff06cecfba1364221c813bcf3fba69
Exadra37 <exadra37@gmail.com>
2016-10-25 22:33:09 +0100

    Exercise 1 - Completed drill 3.

---
+++ exercise-1/ex1-drill3.py
@@ -0,0 +1,9 @@
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'.'"
+print 'I "said" do not touch this.'
+# hash character is used to write comments or to skip executing lines of code
+#print "Drill 1 - Print one more line."
