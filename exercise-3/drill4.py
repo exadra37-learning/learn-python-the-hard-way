dca0dd77bec88cde138bff79213d309cac18f0f8
Exadra37 <exadra37@gmail.com>
2016-10-27 09:06:04 +0100

    Exercise 3 - Drill 4: Demo of the 3 Simple Rule in Maths using floats for accurated results.

---
+++ exercise-3/drill4.py
@@ -0,0 +1,23 @@
+# One of the first things we learn in advanced Mathematics when we go to School in my home country, Portugal
+print "---> 3 Simple Rule in Maths <---\n"
+
+# A good and pratical sample to apply this maths rule is when we want to calculate the average
+#   of fuel consumed by Car by each 100 kms
+print "So lets's imagine that after I have done 892 Kms I fill my car thank and it takes 46 litres.\n"
+print "Now If I want to know how much litres the car spent for each 100 kms I will think like: "
+print "\tFor 46.0 litres in 892 Kms"
+print "\tI spent x litres in 100 Kms\n"
+
+# I am in the forty's so things must be different in School now, but this is as I learned on my time ;)
+print "Translating this to maths in a paper, using old School Style:"
+print "\t     46.0 * 100"
+print "\tx = __________ = 5.156950673"
+print "\t       892   \n"
+
+print "Expected Result: x = ((46.0 * 100) / 892) = 5.156950673"
+
+print "Python Result: x = ", ((46.0 * 100) / 892)
+
+print "\nNow that we have litres as a float, we only have a very small difference, due to the difference in the number of decimals between my PC calculator and Python."
+
+print "\nThis can be easily solved when we learn how to display only a fixed number of decimals and to round them."
