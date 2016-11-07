46d94a814dae2d680b9622b86e0b6436c0f7ee13
Exadra37 <exadra37@gmail.com>
2016-11-07 08:54:28 +0000

    Exercise 5 - Drill 3: Search online for all of the Python format characters.

---
+++ exercise-5/drill3.py
@@ -0,0 +1,8 @@
+# Exercise 5
+#
+# Drill 3:
+#   Search Online for all of the Python format characters
+
+print "Result of Searching Online for Python Format Characters"
+
+print "\n---> My preference goes for this result: https://pyformat.info <---"
