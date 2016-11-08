26306f7203a29b603ef74cad534e62f9bf489fc9
Exadra37 <exadra37@gmail.com>
2016-11-08 08:40:47 +0000

    Exercise 6 - Drill 3: Do we really only have 4 places where we put a string inside another string?

---
+++ exercise-6/drill3.py
@@ -0,0 +1,20 @@
+# Exercise 6
+#
+# Drill 3:
+#   Are you sure there are only four places? How do you Know? Maybe I like lying.
+
+print "\n---> Drill 3 on Exercise 6 <---"
+
+print "\nAre you sure there are only four places where a string is put inside another string?"
+print "- Yes\n"
+
+print "How do you know?"
+
+print "\n- ex6.py in line 4 includes 2 strings, variables (binary, do_not), inside another string:"
+print '\tprint "Those who know %s and those who %s. % (binary, do_not)"'
+print "\n- ex6.py in line 9 includes 1 string, variable x, inside another string:"
+print '\tprint "I said: %r." % x'
+print "\n- ex6.py in line 10 includes 1 string, variable y, inside another string:"
+print '\tprint "I also said: \'%s\'." % y'
+
+print "\nI can't find any more string included inside another string and I count 4 occurrences, so you are not lying ;).\n"
