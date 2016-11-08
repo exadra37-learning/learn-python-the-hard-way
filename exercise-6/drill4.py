93338bfe9df509cd063196c2f9ba0aab988d2fed
Exadra37 <exadra37@gmail.com>
2016-11-08 08:42:36 +0000

    Exercise 6 - Drill 4: Explaining why adding 2 strings with + makes a longer string.

---
+++ exercise-6/drill4.py
@@ -0,0 +1,10 @@
+# Exercise 6
+#
+# Drill 4:
+#   Explain why adding the two strings w and e with + makes a long string.
+
+print "\n---> Drill 4 on Exercise 6 <---"
+
+print "\nExplain why adding the two strings w and e with + makes a long string."
+
+print "- Because Python concatenates both strings in one string.\n"
